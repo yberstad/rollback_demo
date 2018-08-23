74b6be6f6cc9efe209116b544bd04ae96266c752
Oyvind Habberstad <oyvind.habberstad@norwegian.com>
2018-08-23 15:49:45 +0200

    ITMN-1234
    Fixed up more using in startup.cs

--- NAS.Front.DemoApp/NewBreakingFeatureInDev.cs
+++ NAS.Front.DemoApp/NewBreakingFeatureInDev.cs
@@ -1,9 +1,4 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
-
-namespace NAS.Front.DemoApp
+﻿namespace NAS.Front.DemoApp
 {
     public class NewBreakingFeatureInDev
     {
